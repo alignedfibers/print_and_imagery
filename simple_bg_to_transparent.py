93b8512edb0894ec6b76402063b81b4f3883533f
alignedfibers <starsnic@gmail.com>
2025-03-05 07:25:33 -0600

    update

--- simple_bg_to_transparent.py
+++ simple_bg_to_transparent.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 import os
-from rembg import remove
+from rembg import remove, new_session
 from PIL import Image
 
 # Force CPU processing
@@ -13,8 +13,19 @@ def do_removebg(input_path,output_path):
     image = Image.open(input_path)
     # Remove the background
     output = remove(image)
+    output_alpha = remove(
+        image, 
+        alpha_matting=True,  # Enables refined edge detection
+        alpha_matting_foreground_threshold=240,  # Controls what is considered "foreground"
+        alpha_matting_background_threshold=10,  # Helps detect background
+        alpha_matting_erode_size=15  # Reduces excessive removal on soft areas
+    )
+    session = new_session("u2netp")  # Use a lighter, less aggressive model
+    output_u2netp = remove(image, session=session)
+    output_u2netp.save("foreground_removed_u2netp9.png")
     # Save the processed image
     output.save(output_path)
+    output_alpha.save(output_path+"_alpha.png")
     print(f"Background removed and saved as: {output_path}")
 
 if __name__ == "__main__":
